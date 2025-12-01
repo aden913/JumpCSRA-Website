7fb9686d7ae99f57e7eb69457cff8a004ee090cf
Aden913 <coxaden@gmail.com>
2025-12-01 16:47:15 -0500

    updated consent form

--- JumpCSRA/app/routes.ts
+++ JumpCSRA/app/routes.ts
@@ -7,4 +7,5 @@ export default [
   route("profile", "profile.tsx"),
   route("checkout", "routes/checkout.tsx"),
   route("subscription-success", "routes/subscription-success.tsx"),
+  route("sms-consent", "routes/sms-consent.tsx"),
 ] satisfies RouteConfig;
